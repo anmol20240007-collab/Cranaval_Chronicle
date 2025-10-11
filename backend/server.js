451784d1876e281d3c8f1e983e1c2961250480fc
Anmol Kumar <anmolkumar875511@gmail.com>
2025-10-11 07:38:30 +0530

    7:38 AM 11/10/25

--- backend/server.js
+++ backend/server.js
@@ -23,12 +23,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const MONGO_URI = process.env.MONGO_URI;
-if (!MONGO_URI) {
-  throw new Error('MONGODB_URI environment variable not set');
-}
 
-mongoose.connect(MONGO_URI)
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('MongoDB connection error:', err));
 
